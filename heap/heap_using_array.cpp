1b960fd4db3c618ec23f9c828435a7ccd3a77dec
Ankit Banerjee <banerjeeankitmail@gmail.com>
2025-06-10 19:28:05 +0530

    Heap updated

--- heap/heap_using_array.cpp
+++ heap/heap_using_array.cpp
@@ -9,7 +9,7 @@ class max_heap{
             heap.push_back(-1); // 0th index not used for ease in calculation so inserted -1
         }
 
-        void insert(int key){
+        void insert(int key){ //insert one at a time
             heap.push_back(key);
             int temp = key;
             int i = heap.size()-1;
@@ -18,14 +18,15 @@ class max_heap{
                 i = i/2;
             }
             heap[i] = temp;
-            cout<<"Inserted successfully!"<<endl;
+            //cout<<"Inserted successfully!"<<endl;
         }
 
-        void create(vector<int>a){
+        void create(vector<int>a){ // this will clear out existing heap and build a new one
+            heap.clear();
             for(int i=0 ; i<a.size() ; i++){
                 insert(a[i]);
             }
-            cout<<"List created successfully!"<<endl;
+            //cout<<"Heap created successfully!"<<endl;
         }
 
         void disp_heap_array(){
@@ -35,6 +36,56 @@ class max_heap{
             cout<<endl;
         }
 
+        int Delete_max(){
+            if(heap.size() == 1){
+                cout<<"heap is empty!"<<endl;
+            }
+            int n = heap.size()-1;
+            int temp = heap[1];
+            heap[1] =  heap[n];
+            heap[n] = temp;
+            int i,j;
+            i=1;
+            j=i*2;
+            while(j<n-1){
+                if(heap[j]<heap[j+1]){
+                    j=j+1;
+                }
+                if(heap[j]>heap[i]){
+                    temp = heap[i];
+                    heap[i] = heap[j];
+                    heap[j] = temp;
+                    i=j;
+                    j=2*j;
+                }else{
+                    break;
+                }
+            }
+            if(j == n-1){
+                if(heap[i]<heap[j]){
+                    temp = heap[i];
+                    heap[i] = heap[j];
+                    heap[j] = temp;
+                }
+            }
+            temp = heap[n];
+            heap.erase(heap.begin()+n);
+            return temp;
+        }
+
+        void heap_sort(vector<int>&h){
+            create(h);
+            h.clear();
+            while(heap.size() > 1){
+                int temp = Delete_max();
+                h.push_back(temp);
+            }
+        }
+
+        void Delete_heap(){ //to delete the entire heap
+            heap.clear();
+            cout<<"Entire heap cleared! "<<endl;
+        }
         
 };
 
@@ -44,6 +95,15 @@ int main(){
     vector<int> a = {1,4,2,7,40,18,9,10,12,34,23,21};
     h1.create(a);
     h1.disp_heap_array();
+    h1.Delete_max();
+    h1.disp_heap_array();
+    max_heap h2;
+    h2.heap_sort(a);
+    for(int i=0 ; i<a.size(); i++){
+        cout<<a[i]<<"  ";
+    }
+    cout<<endl;
 
-return 0;
+
+    return 0;
 }
